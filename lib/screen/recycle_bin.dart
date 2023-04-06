2a0f7bc8555e05b2908ba18a88b2f4589595d377
RitikRajoria <ritikrajrrr@gmail.com>
2023-04-06 13:40:32 +0530

    added bin functionality

--- lib/screen/recycle_bin.dart
+++ lib/screen/recycle_bin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/bloc/bloc_exports.dart';
 import 'package:notes_app/screen/my_drawer.dart';
 import 'package:notes_app/widgets/tasks_list.dart';
 
@@ -8,32 +9,36 @@ class RecycleBin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Recycle Bin'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.add),
-          )
-        ],
-      ),
-      drawer: MyDrawer(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Chip(
-              label: Text(
-                ' Tasks',
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Recycle Bin'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              )
+            ],
+          ),
+          drawer: MyDrawer(),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Chip(
+                  label: Text(
+                    '${state.removedTasks.length} Tasks',
+                  ),
+                ),
               ),
-            ),
+              Expanded(
+                child: TasksList(tasksList: state.removedTasks),
+              ),
+            ],
           ),
-          Expanded(
-            child: TasksList(tasksList: []),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -10,6 +10,12 @@ class TaskTile extends StatelessWidget {
 
   final Task task;
 
+  void _removeOrDeletetask(BuildContext ctx, Task task) {
+    task.isDeleted!
+        ? ctx.read<TasksBloc>().add(DeleteTask(task: task))
+        : ctx.read<TasksBloc>().add(RemoveTask(task: task));
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -24,7 +30,7 @@ class TaskTile extends StatelessWidget {
         },
         value: task.isDone,
       ),
-      onLongPress: () => context.read<TasksBloc>().add(DeleteTask(task: task)),
+      onLongPress: () => _removeOrDeletetask(context, task),
     );
   }
 }
